76d2363d7e0c2712f79e498a45ee5bebfc1f23ca
Arnaud Gissinger <agissing@student.42.fr>
2020-03-21 14:06:12 +0100

    [ADD] initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import json
+import requests
+from os import getenv
+from notion.client import NotionClient
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+image_size = "380x220"
+labels = ['Done', 'In Progress', 'Todo']
+
+client = NotionClient(token_v2=getenv("TOKEN_V2"))
+URL_BASE = "https://www.notion.so/businesstime/{}?v={}"
+
+def get_stats(collection, view):
+    try:
+        cv = client.get_collection_view(URL_BASE.format(collection, view))
+    except Exception as e:
+        if str(e) == "Invalid collection view URL":
+            raise Exception('Bad view')
+        else:
+            raise
+
+    todo, done, en_cours = 0, 0, 0
+
+    rows = cv.default_query().execute()
+
+    for row in rows:
+        if row.status == "In Progress":
+            en_cours += 1
+        elif row.status == "Not Started":
+            todo += 1
+        else:
+            done += 1
+
+    d = done / len(rows) * 100
+    p = en_cours / len(rows) * 100
+    t = todo / len(rows) * 100
+    return d, p, t
+
+
+@app.route('/robots.txt')
+def robots():
+    return "User-agent: *\nDisallow: /"
+
+
+@app.route('/chart-image/<collection>/<view>')
+def get_chart_image(collection, view):
+    d, p, t = get_stats(collection, view)
+
+    return redirect(f"https://chart.googleapis.com/chart?cht=p&chd=t:{d},{p},{t}&chs={image_size}&chdl={'|'.join(labels)}")
+
+
+@app.route('/chart/<collection>/<view>')
+def get_chart(collection, view):
+    d, p, t = get_stats(collection, view)
+
+    return render_template('chart.html', datas=json.dumps([
+        [labels[0], d],
+        [labels[1], p],
+        [labels[2], t]
+    ]))
+
+
+if __name__ == "__main__":
+    app.run()
